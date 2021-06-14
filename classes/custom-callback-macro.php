752169d3a9f70ad24c2cebf576028ecd6558de7b
Luke Chinworth <luke.chinworth@soliddigital.com>
2021-06-14 12:58:32 -0500

    TECH-449, add plugin code

---
+++ classes/custom-callback-macro.php
@@ -0,0 +1,35 @@
+<?php
+namespace Solid;
+
+class CustomCallbackMacro {
+
+  function __construct() {
+    add_filter('jet-engine/listings/macros-list', [$this, 'macros_list']);
+  }
+
+  function macros_list($macros_list) {
+    $macros_list['custom_callback'] = [
+      'label' => __( 'Custom Callback', 'solid-dynamics' ),
+      'cb'    => [ $this, 'custom_callback' ],
+      'args'  => array(
+        'callback' => array(
+          'label'   => __( 'Callback', 'solid-dynamics' ),
+          'type'    => 'text',
+          'default' => '',
+        ),
+      ),
+    ];
+
+    return $macros_list;
+  }
+
+  function custom_callback($field_value, $callback) {
+    global $post;
+
+    if (is_callable($callback)) {
+      return $callback($post);
+    }
+
+    return $field_value;
+  }
+}
---
+++ classes/parent-meta.php
@@ -0,0 +1,108 @@
+<?php
+namespace Solid;
+
+Class ParentMeta extends \Elementor\Core\DynamicTags\Tag {
+
+    /**
+     * Get Name
+     *
+     * Returns the Name of the tag
+     *
+     * @since 2.0.0
+     * @access public
+     *
+     * @return string
+     */
+    public function get_name() {
+        return 'parent-meta';
+    }
+
+    /**
+     * Get Title
+     *
+     * Returns the title of the Tag
+     *
+     * @since 2.0.0
+     * @access public
+     *
+     * @return string
+     */
+    public function get_title() {
+        return __( 'Parent Meta', 'solid-dynamics' );
+    }
+
+    /**
+     * Get Group
+     *
+     * Returns the Group of the tag
+     *
+     * @since 2.0.0
+     * @access public
+     *
+     * @return string
+     */
+    public function get_group() {
+        return 'solid-dynamics';
+    }
+
+    /**
+     * Get Categories
+     *
+     * Returns an array of tag categories
+     *
+     * @since 2.0.0
+     * @access public
+     *
+     * @return array
+     */
+    public function get_categories() {
+        return [
+            \Elementor\Modules\DynamicTags\Module::TEXT_CATEGORY,
+            \Elementor\Modules\DynamicTags\Module::URL_CATEGORY
+        ];
+    }
+
+    /**
+     * Register Controls
+     *
+     * Registers the Dynamic tag controls
+     *
+     * @since 2.0.0
+     * @access protected
+     *
+     * @return void
+     */
+    protected function _register_controls() {
+
+        $this->add_control(
+            'parent_meta',
+            [
+                'label' => __( 'Parent Meta Key', 'solid-dynamics' ),
+                'type' => 'text',
+            ]
+        );
+    }
+
+    /**
+     * Render
+     *
+     * Prints out the value of the Dynamic tag
+     *
+     * @since 2.0.0
+     * @access public
+     *
+     * @return void
+     */
+    public function render() {
+        $meta_key = $this->get_settings( 'parent_meta' );
+
+        if ( ! $meta_key ) {
+            return;
+        }
+
+        $current = get_post();
+        if ($current && $parent = get_post_parent($current)) {
+            echo implode(",", get_post_meta($parent->ID, $meta_key));
+        }
+    }
+}
