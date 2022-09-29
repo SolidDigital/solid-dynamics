426d21d061da05e73bd8da04757bad437c0c8957
Luke Chinworth <lukechinworth@gmail.com>
2022-09-28 22:08:30 -0500

    update dtag register

--- classes/elementor-dynamic-tags.php
+++ classes/elementor-dynamic-tags.php
@@ -3,7 +3,7 @@ namespace Solid;
 
 class ElementorDynamicTags {
     function __construct() {
-        add_action( 'elementor/dynamic_tags/register_tags', [$this, 'elementor_dynamic_tags_register_tags'] );
+        add_action( 'elementor/dynamic_tags/register', [$this, 'elementor_dynamic_tags_register_tags'] );
     }
 
     function elementor_dynamic_tags_register_tags( $dynamic_tags ) {
@@ -16,9 +16,9 @@ class ElementorDynamicTags {
         include_once( __DIR__ . "/custom-callback.php" );
         include_once( __DIR__ . "/menu.php" );
 
-        $dynamic_tags->register_tag( '\Solid\ParentMeta' );
-        $dynamic_tags->register_tag( '\Solid\ParentMetaImage' );
-        $dynamic_tags->register_tag( '\Solid\CustomCallback' );
-        $dynamic_tags->register_tag( '\Solid\Menu' );
+        $dynamic_tags->register( new \Solid\ParentMeta() );
+        $dynamic_tags->register( new \Solid\ParentMetaImage() );
+        $dynamic_tags->register( new \Solid\CustomCallback() );
+        $dynamic_tags->register( new \Solid\Menu() );
     }
 }
